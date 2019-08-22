1e21d2a3325d44bb330c1a71f9b26d998c02bb1e
WAyling1021 <william.ayling15@gmail.com>
2019-08-22 10:18:57 -0500

    init

---
+++ Models/Athlete.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NextLevel.Models
+{
+    public class Athlete
+    {
+        [Key]
+        public int age { get; set; }
+        public int gradeLevel { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string emailAddress { get; set; }
+        public string schoolName { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int zipCode { get; set; }
+        public string postion { get; set; }
+        public int height { get; set; }
+        public int weight { get; set; }
+
+    }
+}
---
+++ Models/Recruiter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NextLevel.Models
+{
+    public class Recruiter
+    {
+        [Key]
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string emailAddress { get; set; }
+        public string institutionName { get; set; }
+
+    }
+}
